99564c4fcc21b7f3ea28e155e719a6cc1b864d75
LICF01 <lucascubilla@gmail.com>
2022-03-03 02:26:50 -0300

    Added sections and assets

--- src/pages/one.jsx
+++ src/pages/one.jsx
@@ -2,64 +2,176 @@ import { useRef, useEffect } from 'react';
 import Lottie from 'lottie-react';
 import baby from '../lotties/26614-baby-mind-logo-v1.json';
 import EarthOrbitingSun from '../components/EarthOrbitingSun';
-import Stars from '../components/Stars'
+import Stars from '../components/Stars';
 
 import { gsap } from 'gsap';
-import { Flip } from 'gsap/Flip';
-import { motion } from 'framer-motion';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { TextPlugin } from 'gsap/TextPlugin';
 
-gsap.registerPlugin(Flip);
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 const One = () => {
-	const hello = useRef();
-	const text2 = useRef();
-	const babyLottie = useRef();
-	const earth = useRef();
-	const tl = useRef();
+	const tl = gsap.timeline();
+	const info = useRef(null);
+	const babyAnim = useRef(null);
+	const earthAnim = useRef(null);
+	const cloudLeft = useRef(null);
+	const cloudRight = useRef(null);
+	const planetSurface = useRef(null);
+	const astronaut = useRef(null);
+
+	// this handles the fade effect of all text
+	useEffect(() => {
+		const textRefs = gsap.utils.toArray('.section');
+		textRefs.forEach((el) => {
+			tl.current = gsap.timeline().fromTo(
+				el.querySelectorAll('.text'),
+				{
+					opacity: 0,
+				},
+				{
+					opacity: 1,
+					ease: 'ease',
+					scrollTrigger: {
+						trigger: el,
+						scrub: true,
+						start: 'top center',
+						end: 'top top',
+					},
+				}
+			);
+		});
+	}, []);
 
 	useEffect(() => {
-		tl.current = gsap
-			.timeline()
-			.to(babyLottie.current, {
+		tl.current.from(babyAnim.current, {
+			opacity: 0,
+			height: '60rem',
+			width: '60rem',
+			duration: 3,
+		});
+	});
+
+	useEffect(() => {
+		tl.current.fromTo(
+			earthAnim.current,
+			{
 				opacity: 1,
-				height: '19rem',
-				width: '20rem',
-				duration: 2,
+				width: '200vw',
+			},
+			{
+				opacity: 1,
+				width: '100vw',
+				duration: 1,
+				scrollTrigger: {
+					trigger: earthAnim.current,
+					scrub: true,
+					start: 'top 30%',
+					end: 'top top',
+				},
+			}
+		);
+	});
+
+	useEffect(() => {
+		tl.current = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.section-3',
+				pin: true, // pin the trigger element while active
+				start: 'top top', // when the top of the trigger hits the top of the viewport
+				end: '+=500', // end after scrolling 500px beyond the start
+				scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+			},
+		});
+		tl.current
+			.from(cloudLeft.current, {
+				xPercent: -100,
+				ease: 'ease',
+				duration: 1,
 			})
-			.to(hello.current, { opacity: 1, ease: 'ease', duration: 1 })
-			.to(text2.current, { opacity: 1, ease: 'ease', duration: 1 });
+			.from(cloudRight.current, { x: '150vw', ease: 'ease', duration: 1 })
+			.from(planetSurface.current, {
+				y: '110vh',
+				ease: 'ease',
+				duration: 1,
+			})
+			.from(astronaut.current, {
+				y: '110vh',
+				ease: 'ease',
+				duration: 1,
+			});
 	});
 
 	return (
 		<>
-			<div className='flex items-center justify-center flex-col  w-[100vw] h-[100vh]'>
-				<p ref={hello} className='text-7xl font-bold opacity-0'>
-					Hola!
-				</p>
-				<div ref={babyLottie} className='opacity-0 h-[50rem] w-[50rem]'>
+			<div className='section bg-space-dark flex items-center justify-center flex-col  w-[100vw] h-[100vh] text-slate-300'>
+				<p className='text text-7xl font-bold'>Hola!</p>
+				<div ref={babyAnim} className='baby h-[20rem] w-[20rem]'>
 					<Lottie loop={true} animationData={baby} />
 				</div>
-				<div ref={text2} className='p-10 opacity-0'>
-					<p className='text-3xl'>
-						Tengo una gran noticia que compartir contigo
+				<div className='px-10'>
+					<p className='text text-3xl text-slate-300 font-bold'>
+						Tengo una gran noticia que compartir!!!
 					</p>
 				</div>
 			</div>
-			<div className='bg-space-dark flex items-center justify-center flex-col w-[100vw] h-[100vh]'>
+			<div className='section bg-space-dark flex items-center justify-center flex-col w-[100vw] h-[100vh]'>
 				<div className='px-10'>
-					<p className='text-3xl text-slate-300 font-bold'>
+					<p className='text text-3xl text-slate-300 font-bold'>
 						Estoy por completar mi primera vuelta al sol
 					</p>
 				</div>
-				<div ref={earth}>
+				<div ref={earthAnim}>
 					<EarthOrbitingSun />
 				</div>
 				<div className='px-10'>
-					<p className='text-3xl text-slate-300 font-bold'>
-						Me acompanaras a festejar este momento especial?
+					<p className='text text-3xl text-slate-300 font-bold'>
+						Me acompañarías a festejar este momento especial?
 					</p>
 				</div>
 				<Stars />
+			</div>
+			<div className='section section-3 bg-space-dark flex items-center justify-center flex-col w-[100vw] h-[100vh] relative overflow-hidden'>
+				<div className='flex flex-col justify-center items-center text-center px-10 absolute top-[120px]'>
+					<p className='text text-3xl text-slate-300 font-bold py-2'>
+						Te espero el sábado 12 de Marzo de 2022 en la sucursal 2
+						de Navegando a las 17:00
+					</p>
+					<p className='text text-3xl text-slate-300 font-bold py-2'>
+						Tte. 1º Porfirio Saldivar Nº 277
+					</p>
+					<p className='text text-3xl text-slate-300 font-bold py-2'>
+						Nos vemos!
+					</p>
+				</div>
+				<div
+					ref={cloudLeft}
+					className='absolute w-[50vw] h-[50vw] bottom-[-10vw] left-[-10vw] z-30'
+				>
+					<img src='./SVG/cloudLeft.svg' className='w-[100%]' />
+				</div>
+				<div
+					ref={cloudRight}
+					className='absolute w-[50vw] h-[50vw] bottom-[-10vw] right-[-10vw] z-20'
+				>
+					<img src='./SVG/cloudRight.svg' className='w-[100%]' />
+				</div>
+				<div
+					ref={planetSurface}
+					className='absolute w-[100vw] bottom-[0] z-0'
+				>
+					<img src='./SVG/planetSurface.svg' className='w-[100%]' />
+				</div>
+				<div
+					ref={astronaut}
+					className='absolute w-[40vw] bottom-[80px] z-0'
+				>
+					<img
+						src='./SVG/astronautWithFlag.svg'
+						className='w-[100%]'
+					/>
+				</div>
 			</div>
 		</>
 	);
