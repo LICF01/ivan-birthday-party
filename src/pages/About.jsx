f75d564588b0c092c7dbc70e4b0013b605fb7cef
LICF01 <lucascubilla@gmail.com>
2022-03-04 10:46:14 -0300

    Centered content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,34 +21,34 @@ const About = () => {
 	}, []);
 
 	return (
-		<div className='px-10 min-h-screen py-20 relative'>
-			<div>
-				<Link to='/'>
-					<p className='text text-md font-bold absolute top-0 mt-5 uppercase'>
-						Atras
-					</p>
-				</Link>
-			</div>
-			<p className='text text-5xl font-bold mx-auto mt-20'>Para Iván y Asu</p>
-
-			<div ref={ref} className='h-[20rem] w-[20rem] mx-auto'>
-				<Lottie loop={true} animationData={WorkingAstronaut} />
-			</div>
-
-			<div className='info  text-center text-2xl font-bold'>
-				<p className='text  my-5'>
-					Hecho por
-					<a
-						href='https://github.com/LICF01'
-						class='text-blue-400 visited:text-purple-400 ...'
-					>
-						{' '}
-						Lucas Cubilla{' '}
-					</a>
-					con React, TailwindCSS y Gsap
+		<>
+			<Link to='/'>
+				<p className='text text-md font-bold absolute top-0 mt-5 uppercase ml-10'>
+					Atras
 				</p>
+			</Link>
+			<div className='px-10 flex justify-center items-center flex-col min-h-screen py-20 relative'>
+				<p className='text text-5xl font-bold'>Para Iván y Asu</p>
+
+				<div ref={ref} className='h-[20rem] w-[20rem] mx-10 '>
+					<Lottie loop={true} animationData={WorkingAstronaut} />
+				</div>
+
+				<div className='info  text-center text-2xl font-bold'>
+					<p className='text  my-5'>
+						Hecho por
+						<a
+							href='https://github.com/LICF01'
+							class='text-blue-400 visited:text-purple-400 ...'
+						>
+							{' '}
+							Lucas Cubilla{' '}
+						</a>
+						con React, TailwindCSS y Gsap
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
