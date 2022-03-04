ed75dbefd5f80714b7026d23e3255940a4caf979
LICF01 <lucascubilla@gmail.com>
2022-03-03 21:37:55 -0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import One from './sections/One';
-import Two from './sections/Two';
-import Three from './sections/Three';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Mercury from './pages/Mercury';
+import Venus from './pages/Venus';
+import Earth from './pages/Earth';
 
 // tsParticles
 import Stars from './components/Stars';
 
 function App() {
 	return (
-		<div className='text-center'>
-			<One />
-			<Two />
-			<Three />
-			<div className='z-0'>
-				<Stars />
+			<div className='App bg-space-dark text-slate-300 text-center '>
+				<Routes>
+					 <Route path='/' element={<Home />} />
+					 <Route path='/mercury' element={<Mercury />} />
+					 <Route path='/venus' element={<Venus />} />
+					 <Route path='/venus' element={<Earth />} />
+				</Routes>
+				<div className='z-0'>
+					<Stars />
+				</div>
 			</div>
-		</div>
 	);
 }
 
